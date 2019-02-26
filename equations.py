bdacb1a9b2c4d1554fc1c870d9c23a552397e654
gitrous <37446909+gitrous@users.noreply.github.com>
2019-02-26 08:39:14 -0500

    Added RPM to MPS conversion
    Added revolutions per minute to meters per second conversion.

--- equations.py
+++ equations.py
@@ -1,3 +1,18 @@
+#Revolutions per minute to meters per second
+def rpm_to_mps(rpm,radius):
+    print("RPM =",rpm)
+    print("r =", radius)
+    #Calculate the circumference
+    circum = radius * 2 * 3.14
+    print("C =", circum)
+
+    #Calculate the meters per minute
+    mpm = rpm * circum
+    print("MPM =", mpm)
+
+    #Calculate the meters per second
+    return mpm/60
+
 #Universal Gravitation
 def universal_gravitation(mass1,mass2,distance,grav = 6.67 * (10**-11)):
     print("M1 =", mass1)
