f9db867e78daa0c56e09138ca87068037757e0a8
gitrous <37446909+gitrous@users.noreply.github.com>
2019-02-26 08:45:17 -0500

    Added MPS to RPM and Circumference
    Added MPS to RPM conversions, and put circumference into a function

--- equations.py
+++ equations.py
@@ -1,9 +1,30 @@
+#Calculate the circumference of a circle
+def circumference(radius):
+    return radius * 2 * 3.14
+
+#Meters per second to revolutions per minute
+def mps_to_rpm(mps,radius):
+    print("MPS =", mps)
+    print("r =", radius)
+
+    #Calculate the circumference
+    circum = circumference(radius)
+    print("C =", circum)
+
+    #Calculate the meters per minute
+    mpm = mps * 60
+    print("MPM =", mpm)
+
+    #Calculate the revolutions per minute
+    return mpm/circum
+
 #Revolutions per minute to meters per second
 def rpm_to_mps(rpm,radius):
     print("RPM =",rpm)
     print("r =", radius)
+    
     #Calculate the circumference
-    circum = radius * 2 * 3.14
+    circum = circumference(radius)
     print("C =", circum)
 
     #Calculate the meters per minute
